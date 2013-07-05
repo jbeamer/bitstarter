c2a597cfb82933f4390d26feaa46cf8856d8dd13
Jonathan Beamer <jbeamer@gmail.com>
2013-07-05 17:53:24 +0000

    Fixed grader to accept URL as input

--- grader.js
+++ grader.js
@@ -24,28 +24,30 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest    = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1);
+        console.log("%s does not exist as local file, checking as URL", instr);
+	process.exit(1);
     }
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlfileContents) {
+    return cheerio.load(htmlfileContents);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfileContents, checksfile) {
+    $ = cheerioHtmlFile(htmlfileContents);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -55,14 +57,39 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var processFile = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+	/* might be a URL instead */
+	rest.get(instr).on('complete', performChecksHTML);
+    } else {
+	var contents = fs.readFileSync(instr);
+	var checkJson = checkHtmlFile(contents, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+};
+
+var performChecksHTML = function(result, response) {
+    if (result instanceof Error) {
+	console.error('Error: ' + util.format(response.message));
+	process.exit(1);
+    } else {
+	var checkJson = checkHtmlFile(result, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+};
+
+var clone = function(fn) {
+    return fn.bind({});
+};
+
 if(require.main == module) {
     program
-        .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-        .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json',        clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>',   'Path (or URL) to index.html', clone(processFile), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
